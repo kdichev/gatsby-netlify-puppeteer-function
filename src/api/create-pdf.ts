988391b339d94db4e255e72a186329267064fade
kdichev <konstantin.dichev@gmail.com>
2023-05-02 22:50:11 +0200

    revert

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -1,13 +1,11 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
-import { join } from "path";
 
 export default async function createPdf(
   _: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
-  console.log(__dirname, process.cwd(), process.env.PUPPETEER_CACHE_DIR);
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
