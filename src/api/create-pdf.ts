7a7eeb46503f406e183ee6f135e599124edca028
kdichev <konstantin.dichev@gmail.com>
2023-05-05 20:17:58 +0200

    add log

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -1,14 +1,12 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
-const dirTree = require("directory-tree");
 
 export default async function createPdf(
   _: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
-  console.log(process.cwd());
-  const tree = dirTree(process.cwd());
+  console.log("AWS_EXECUTION_ENV", process.env.AWS_EXECUTION_ENV);
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -25,5 +23,5 @@ export default async function createPdf(
   const pdf = await page.pdf({ format: "A4" });
   console.log(pdf);
   await browser.close();
-  res.status(200).json({ success: true, tree, pdf });
+  res.status(200).json({ success: true, pdf });
 }
