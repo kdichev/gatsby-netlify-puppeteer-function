d8c4cc6d00ec01d15b9a327e8544a7be3625c4fe
kdichev <konstantin.dichev@gmail.com>
2023-05-05 20:04:22 +0200

    test

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -9,19 +9,21 @@ export default async function createPdf(
 ) {
   console.log(process.cwd());
   const tree = dirTree(process.cwd());
-  // const browser = await puppeteer.launch({
-  //   args: chromium.args,
-  //   defaultViewport: chromium.defaultViewport,
-  //   executablePath: await chromium.executablePath(),
-  //   headless: chromium.headless,
-  // });
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(
+      "/var/task/node_modules/@sparticuz/chromium/bin"
+    ),
+    headless: chromium.headless,
+  });
 
-  // const page = await browser.newPage();
-  // await page.goto("https://www.example.com", {
-  //   waitUntil: "networkidle0",
-  // });
-  // const pdf = await page.pdf({ format: "A4" });
-  // console.log(pdf);
-  // await browser.close();
+  const page = await browser.newPage();
+  await page.goto("https://www.example.com", {
+    waitUntil: "networkidle0",
+  });
+  const pdf = await page.pdf({ format: "A4" });
+  console.log(pdf);
+  await browser.close();
   res.status(200).json({ success: true, tree });
 }
