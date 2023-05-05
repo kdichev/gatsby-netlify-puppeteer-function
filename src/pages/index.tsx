dd9a04bc51eba813c2131618143986d56a9cebae
kdichev <konstantin.dichev@gmail.com>
2023-05-05 21:21:42 +0200

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,7 @@ const IndexPage = () => {
   const [loading, setLoading] = React.useState(false);
   const getPdf = async () => {
     setLoading(true);
-    // gatsby function
     await fetch("./api/create-pdf");
-    // netlify function
-    // await fetch("/.netlify/functions/test");
     setLoading(false);
   };
   return (
