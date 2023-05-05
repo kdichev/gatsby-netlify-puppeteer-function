5a119e70817c043905c12067fbd243a1b1cd3e43
kdichev <konstantin.dichev@gmail.com>
2023-05-05 19:45:36 +0200

    add

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -1,37 +1,24 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
-import fs from "fs";
-import os from "os";
-const dirTree = require("directory-tree");
-
-const glob = require("glob");
 
 export default async function createPdf(
   _: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
-  // console.log("process.cwd()", process.cwd());
-  // fs.readdir(process.cwd() + "/.cache/functions", (err, files) => {
-  //   files.forEach((file) => {
-  //     console.log(file);
-  //   });
-  // });
-  const tree = dirTree(process.cwd());
-  console.log(tree);
-  // const browser = await puppeteer.launch({
-  //   args: chromium.args,
-  //   defaultViewport: chromium.defaultViewport,
-  //   executablePath: await chromium.executablePath(),
-  //   headless: chromium.headless,
-  // });
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+  });
 
-  // const page = await browser.newPage();
-  // await page.goto("https://www.example.com", {
-  //   waitUntil: "networkidle0",
-  // });
-  // const pdf = await page.pdf({ format: "A4" });
-  // console.log(pdf);
-  // await browser.close();
-  res.status(200).json({ success: true, tree });
+  const page = await browser.newPage();
+  await page.goto("https://www.example.com", {
+    waitUntil: "networkidle0",
+  });
+  const pdf = await page.pdf({ format: "A4" });
+  console.log(pdf);
+  await browser.close();
+  res.status(200).json({ success: true });
 }
