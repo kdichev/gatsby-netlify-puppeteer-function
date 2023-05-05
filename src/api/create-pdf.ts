e9a4b28712754e0d0a8595721d8feaf706bb997e
kdichev <konstantin.dichev@gmail.com>
2023-05-05 17:57:48 +0200

    test

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -1,13 +1,23 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium-min");
+import os from "os";
+const fs = require("fs");
 
 console.log(process.env.AWS_LAMBDA_JS_RUNTIME);
+console.log(console.log(os.tmpdir()));
+
+fs.readdirSync(os.tmpdir()).forEach((file) => {
+  console.log(file);
+});
 
 export default async function createPdf(
   _: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
+  fs.readdirSync(os.tmpdir()).forEach((file) => {
+    console.log(file);
+  });
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
