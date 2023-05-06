6ece9a59a63ad67ef3cf07e7d33755157b421756
kdichev <konstantin.dichev@gmail.com>
2023-05-06 20:12:06 +0200

    test

--- src/api/create-pdf.ts
+++ src/api/create-pdf.ts
@@ -1,6 +1,8 @@
 import { GatsbyFunctionRequest, GatsbyFunctionResponse } from "gatsby";
 const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium-min");
+const os = require("os");
+const dirTree = require("directory-tree");
 
 export default async function createPdf(
   _: GatsbyFunctionRequest,
@@ -13,21 +15,32 @@ export default async function createPdf(
     process.env["AWS_LAMBDA_JS_RUNTIME"] &&
       /^nodejs/.test(process.env["AWS_LAMBDA_JS_RUNTIME"]) === true
   );
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      "https://github.com/Sparticuz/chromium/releases/download/v113.0.1/chromium-v113.0.1-pack.tar"
-    ),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.example.com", {
-    waitUntil: "networkidle0",
-  });
-  const pdf = await page.pdf({ format: "A4" });
-  console.log(pdf);
-  await browser.close();
-  res.status(200).json({ success: true });
+  console.log(os.tmpdir());
+  const tree = dirTree(os.tmpdir());
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(
+        "https://github.com/Sparticuz/chromium/releases/download/v113.0.1/chromium-v113.0.1-pack.tar"
+      ),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto("https://www.example.com", {
+      waitUntil: "networkidle0",
+    });
+    const pdf = await page.pdf({ format: "A4" });
+    console.log(pdf);
+    await browser.close();
+    res.status(200).json({ success: true, pdf });
+  } catch (e) {
+    res.status(200).json({
+      tree,
+      error: JSON.stringify({
+        message: e instanceof Error ? e.message : "some error happened",
+      }),
+    });
+  }
 }
